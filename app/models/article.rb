8288d8efc7e525e87322ce99f6f8e66ecb3f3be8
Krapi Rastogi <krapi.rastogi@reddoorz.com>
2024-03-05 10:08:34 +0530

    added devise gem and table on home page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 include Visible
+  belongs_to :articles
   has_many:comments , dependent: :destroy
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
